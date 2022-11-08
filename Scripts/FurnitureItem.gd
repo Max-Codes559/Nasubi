2caed67bfc97acbea6009bb3e2220b82672da6ff
Max <max4sure@ymail.com>
2022-11-07 23:44:50 -0800

    Added Y sorting, furniture now goes to Floor Y sort node after being instanced

--- Scripts/FurnitureItem.gd
+++ Scripts/FurnitureItem.gd
@@ -2,29 +2,42 @@ extends StaticBody2D
 
 onready var collisionBody = $CollisionShape2D
 onready var sprite = $Sprite
-
-var size
+onready var ysort = get_parent().get_parent().get_parent()
+var slot
+var dataRecieved = false
 
 func get_resource_data(item):
-	sprite.texture = item.texture
+	if dataRecieved == false:
+		sprite.texture = item.texture
 	
-	if item.size == "1x1":
-		sprite.position = get_parent().rect_size / 2
-		collisionBody.position = get_parent().rect_size / 2
+		if item.size == "1x1":
+			sprite.position = slot.rect_size / 2
+			collisionBody.position = slot.rect_size / 2
 		
-	elif item.size == "2x1":
-		collisionBody.scale *= Vector2(3.6,2)
-		collisionBody.position.x = get_parent().rect_size.x
-		collisionBody.position.y = get_parent().rect_size.y / 2
-		sprite.position.x = get_parent().rect_size.x
-		sprite.position.y = get_parent().rect_size.y / 2
+		elif item.size == "2x1":
+			collisionBody.scale *= Vector2(3.6,2)
+			collisionBody.position.x = slot.rect_size.x
+			collisionBody.position.y = slot.rect_size.y / 2
+			sprite.position.x = slot.rect_size.x
+			sprite.position.y = slot.rect_size.y / 2
 		
-	elif item.size == "2x2":
-		collisionBody.scale *= Vector2(2,2)
+		elif item.size == "2x2":
+			collisionBody.scale *= Vector2(2,2)
 		
-	elif item.size == "rug":
-		collisionBody.set_deferred("disabled", true)
+		elif item.size == "rug":
+			collisionBody.set_deferred("disabled", true)
+		
+		dataRecieved = true
+
+func transfer_node():
+	var slotPosition = global_position
+	var slotNumber = get_parent().get_index()
+	get_parent().remove_child(self)
+	ysort.add_child(self)
+	global_position = slotPosition
 
 func _ready():
-	var slot = get_parent()
-	slot.connect("furniture_type", self, "get_resource_data")
+	if dataRecieved == false:
+		slot = get_parent()
+		slot.connect("furniture_type", self, "get_resource_data")
+		transfer_node()
