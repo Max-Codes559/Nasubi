70c617408ccd8aaa1203c6e488e705d43ec5d330
Max <max4sure@ymail.com>
2022-12-19 01:07:31 -0800

    Added date change and seasons

--- Scenes/SleepMenu.gd
+++ Scenes/SleepMenu.gd
@@ -4,6 +4,5 @@ func _on_NoButton_button_down():
 	visible = false
 
 func _on_YesButton_button_down():
-	global.date += Vector2(0, 1)
+	global.date = Vector2(0, 1)
 	visible = false
-	print(global.date)
--- Scripts/global.gd
+++ Scripts/global.gd
@@ -4,5 +4,16 @@ var enteredContests = []
 var contestDetailDict= {1 : "contest 1 data"}
 var minigameScore = 0
 
-var date = Vector2(1, 1)
+var monthLength = 4
+#28 days after testing
+var date = Vector2(1, 1) setget set_date
 #month, day
+
+func set_date(addDate):
+	date += addDate
+	if date.y > monthLength:
+		date.y = 1
+		date.x += 1
+		if date.x > 4:
+			date.x = 1
+	print(date)
